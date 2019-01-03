1167f41379e4993076300aff8ebec41bed403751
Edilson do Nascimento <edilsonjoper@gmail.com>
2019-01-03 12:04:26 -0200

    03 - Trabalhando com parâmetros

--- src/test/java/br/com/alura/loja/ProjetoTest.java
+++ src/test/java/br/com/alura/loja/ProjetoTest.java
@@ -4,19 +4,40 @@ import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
 
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
+import com.thoughtworks.xstream.XStream;
+
+import br.com.alura.loja.modelo.Carrinho;
+import br.com.alura.loja.modelo.Projeto;
+
 public class ProjetoTest {
+	
+	HttpServer server;
+	
+	@Before
+	public void startServidor() {
+		this.server = Servidor.inicializaServidor();
+	}
+	
+	@After
+	public void mataServidor() {
+		System.out.println("SERVIDOR PARADO ...");
+		server.stop();
+	}
 	
 	@Test
 	public void testaQueAConexaoComOServidorNoPathProjetosFuncina() {
 		
 		Client client = ClientBuilder.newClient();
 		WebTarget target = client.target("http://localhost:8080");
-		String conteudo = target.path("/projetos").request().get(String.class);
-		System.out.println(conteudo);
-		Assert.assertTrue(conteudo.contains("<nome>Minha loja"));
+		String conteudo = target.path("/projetos/1").request().get(String.class);
+		Projeto projeto = (Projeto) new XStream().fromXML(conteudo);
+		Assert.assertEquals("Minha loja", projeto.getNome());
 	}
 
 }
